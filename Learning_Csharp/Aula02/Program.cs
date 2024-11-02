45c1b4a9bcfc4f06a52b9fc341c724d358707a52
nicolasgghg <nicolasgghg12@gmail.com>
2024-11-02 14:44:42 -0300

    feat: learning dotnet

---
+++ Learning_Csharp/Aula02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Hello, what's your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {name}! Welcome to my first project.");
+
+            Console.WriteLine("How old are you?");
+            int yearsOld;
+
+            while (true)
+            {
+                string yearsOldInput = Console.ReadLine();
+                if (Int32.TryParse(yearsOldInput, out yearsOld))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid age:");
+                }
+            }
+
+            if (yearsOld < 18)
+            {
+                Console.WriteLine("You are underage.");
+            }
+            else
+            {
+                Console.WriteLine("You are of legal age.");
+            }
+
+            Console.WriteLine("Do you want to continue? (y/n)");
+            string continueInput = Console.ReadLine();
+
+            if (continueInput.ToLower() != "y")
+            {
+                break;
+            }
+        }
+    }
+}
